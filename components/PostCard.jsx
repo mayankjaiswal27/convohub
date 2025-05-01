99deb931f200c0ca8cdc14f03e30c9171a146e86
mayankjaiswal27 <mayankjaiswal0104@gmail.com>
2025-05-01 17:23:14 +0530

    Changes pushed
    Co-Authored-By: Harshita Khare <harshitashirsh@gmail.com>

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,66 +1,53 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { theme } from '../constants/theme'
-import { hp,wp } from '../helpers/common'
-import Avatar from './Avatar'
-import moment from 'moment'
-import Icon from '../assets/icons'
-import RenderHTML from 'react-native-render-html'
-import { Image} from 'expo-image'
-import { getSupabaseFileUrl } from '../services/imageService'
-import { Video } from 'expo-av'
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Share } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { theme } from '../constants/theme';
+import { hp, wp } from '../helpers/common';
+import Avatar from './Avatar';
+import moment from 'moment';
+import Icon from '../assets/icons';
+import RenderHTML from 'react-native-render-html';
+import { Image } from 'expo-image';
+import { getSupabaseFileUrl } from '../services/imageService';
+import { Video } from 'expo-av';
+import { createPostLike, removePostLike } from '../services/postServices';
+import { stripHtmlTags } from '../helpers/common';
+import { downloadFile } from '../services/imageService';
+import Loading from './Loading';
 
-const textStyle={
-  color:theme.colors.dark,
-  fontSize:hp(1.75),
-}
-const tagsStyles={
-  div:textStyle,
-  p:textStyle,
-  ol:textStyle,
-  h1:{
-    color:theme.colors.dark,
+const textStyle = {
+  color: theme.colors.dark,
+  fontSize: hp(1.75),
+};
+
+const tagsStyles = {
+  div: textStyle,
+  p: textStyle,
+  ol: textStyle,
+  h1: {
+    color: theme.colors.dark,
   },
-<<<<<<< HEAD
   h4: {
     color: theme.colors.dark,
   },
 };
-=======
-  h4:{
-    color:theme.colors.dark,
-  }
-}
 
->>>>>>> parent of c2ddf1b (Like, Share & Pagination)
-const PostCard = ({
-  item,
-  currentUser,
-  router,
-<<<<<<< HEAD
-  hasShadow=true,
-  showMoreIcon=true,
-}) => {
-  const [likes, setLikes] = useState([]);
+const PostCard = ({ item, currentUser, router, hasShadow = true }) => {
+  const [likes, setLikes] = useState(item?.postLikes || []);
   const [loading, setLoading] = useState(false);
-=======
-  hasShadow=true
-}) => {
->>>>>>> parent of c2ddf1b (Like, Share & Pagination)
 
-  const shadowStyles={
-    shadowOffset:{
-      width:0,
-      height:2
+  const shadowStyles = {
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-<<<<<<< HEAD
     shadowOpacity: 0.06,
     shadowRadius: 6,
     elevation: 1,
   };
+
   useEffect(() => {
-    setLikes(item?.postLikes);
-  }, []);
+    setLikes(item?.postLikes || []);
+  }, [item?.postLikes]);
 
   const created_at = moment(item?.created_at).format('MMM D');
 
@@ -77,26 +64,25 @@ const PostCard = ({
   };
 
   const openPostDetails = () => {
-   router.push({ pathname: 'postDetails', params: { postId: item?.id } });
-
+    router.push({ pathname: 'postDetails', params: { postId: item?.id } });
   };
 
   const onLike = async () => {
     if (liked) {
-      let updatedLikes = likes.filter((like) => like.userId !== currentUser?.id);
+      const updatedLikes = likes.filter((like) => like.userId !== currentUser?.id);
       setLikes([...updatedLikes]);
-      let res = await removePostLike(item?.id, currentUser?.id);
+      const res = await removePostLike(item?.id, currentUser?.id);
       console.log('removed likes', res);
       if (!res.success) {
         Alert.alert('Post', 'Something went wrong');
       }
     } else {
-      let data = {
+      const data = {
         userId: currentUser?.id,
         postId: item?.id,
       };
       setLikes([...likes, data]);
-      let res = await createPostLike(data);
+      const res = await createPostLike(data);
       console.log('added like', res);
       if (!res.success) {
         Alert.alert('Post', 'Something went wrong');
@@ -104,180 +90,142 @@ const PostCard = ({
     }
   };
 
-  const liked = likes.some((like) => like.userId === currentUser?.id);
-=======
-    shadowOpacity:0.06,
-    shadowRadius:6,
-    elevation:1
-  }
+  const liked = Array.isArray(likes) && likes.some((like) => like.userId === currentUser?.id);
 
-  const created_at=moment(item?.created_at).format('MMM D');
-  const openPostDetails=()=>{
-
-  }
-  const liked=false;
-  const likes=[];
->>>>>>> parent of c2ddf1b (Like, Share & Pagination)
   return (
-    <View style={[styles.container, hasShadow && shadowStyles ]}>
+    <View style={[styles.container, hasShadow && shadowStyles]}>
       <View style={styles.header}>
         <View style={styles.userInfo}>
-          <Avatar
-          size={hp(4.5)}
-          uri={item?.user?.image}
-          rounded={theme.radius.md}
-          />
-          <View style={{gap:2}}>
+          <Avatar size={hp(4.5)} uri={item?.user?.image} rounded={theme.radius.md} />
+          <View style={{ gap: 2 }}>
             <Text style={styles.username}>{item?.user?.name}</Text>
             <Text style={styles.postTime}>{created_at}</Text>
           </View>
-
         </View>
         <TouchableOpacity onPress={openPostDetails}>
-          <Icon name="threeDotsHorizontal" size={hp(3.4)} color={theme.colors.text} strokeWidth={3}/>
+          <Icon name="threeDotsHorizontal" size={hp(3.4)} color={theme.colors.text} strokeWidth={3} />
         </TouchableOpacity>
       </View>
-      {
-            showMoreIcon && (
-              <TouchableOpacity onPress={openPostDetails}>
-                <Icon name="threeDotsHorizontal" size={hp(3.4)} color={theme.colors.text} strokeWidth={3}/>
-              </TouchableOpacity>
-            ) 
-          }
       <View style={styles.content}>
         <View style={styles.postBody}>
-          {
-            item?.body && (
-              <RenderHTML
-                contentWidth={wp(100)}
-                source={{html:item?.body}}
-                tagsStyles={tagsStyles}
-              />
-            )
-          }
+          {item?.body && (
+            <RenderHTML
+              contentWidth={wp(100)}
+              source={{ html: item?.body }}
+              tagsStyles={tagsStyles}
+            />
+          )}
         </View>
-        {
-          item?.file && item?.file?.includes('postImages') &&(
-            <Image
+        {item?.file && item?.file?.includes('postImages') && (
+          <Image
             source={getSupabaseFileUrl(item?.file)}
             transition={100}
             style={styles.postMedia}
-            contentFit='cover'
-            />
-          )
-        }
-        {
-          item?.file && item?.file?.includes('postVideos') &&(
-            <Video
-            style={[styles.postMedia,{height:hp(30)}]}
-            source={
-              {uri:getSupabaseFileUrl(item?.file)}}
+            contentFit="cover"
+          />
+        )}
+        {item?.file && item?.file?.includes('postVideos') && (
+          <Video
+            style={[styles.postMedia, { height: hp(30) }]}
+            source={{ uri: getSupabaseFileUrl(item?.file) }}
             useNativeControls
-            resizeMode='cover'
+            resizeMode="cover"
             isLooping
-            />
-          )
-        }
+          />
+        )}
       </View>
       <View style={styles.footer}>
         <View style={styles.footerButton}>
-          <TouchableOpacity>
-            <Icon name="heart" size={22} fill={liked?theme.colors.rose:"transparent"} color={liked?theme.colors.rose:theme.colors.textLight} strokeWidth={2}/>
+          <TouchableOpacity onPress={onLike}>
+            <Icon
+              name="heart"
+              size={22}
+              fill={liked ? theme.colors.rose : 'transparent'}
+              color={liked ? theme.colors.rose : theme.colors.textLight}
+              strokeWidth={2}
+            />
           </TouchableOpacity>
-          <Text style={styles.count}>
-            {likes?.length}
-          </Text>
+          <Text style={styles.count}>{likes?.length}</Text>
         </View>
-        <View style={styles.footerButton}>
-          <TouchableOpacity>
-            <Icon name="comment" size={22} color={theme.colors.textLight}/>
+        {/* <View style={styles.footerButton}>
+          <TouchableOpacity onPress={openPostDetails}>
+            <Icon name="comment" size={22} color={theme.colors.textLight} />
           </TouchableOpacity>
-          <Text style={styles.count}>
-            {likes?.length}
-          </Text>
-        </View>
+          <Text style={styles.count}>{likes?.length}</Text>
+        </View> */}
         <View style={styles.footerButton}>
-          <TouchableOpacity>
-            <Icon name="share" size={22} color={theme.colors.textLight}/>
-          </TouchableOpacity>
-
+          {loading ? (
+            <Loading size="small" />
+          ) : (
+            <TouchableOpacity onPress={onShare}>
+              <Icon name="share" size={22} color={theme.colors.textLight} />
+            </TouchableOpacity>
+          )}
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
 
 const styles = StyleSheet.create({
-  container:{
-    gap:10,
-    marginBottom:15,
-    borderRadius:theme.radius.xxl*1.1,
-    borderCurve:'continuous',
-    padding:10,
-    paddingVertical:12,
-    backgroundColor:"white",
-    borderWidth:0.5,
-    borderColor:theme.colors.gray,
-    shadowColor:'#000',
+  container: {
+    gap: 10,
+    marginBottom: 15,
+    borderRadius: theme.radius.xxl * 1.1,
+    borderCurve: 'continuous',
+    padding: 10,
+    paddingVertical: 12,
+    backgroundColor: 'white',
+    borderWidth: 0.5,
+    borderColor: theme.colors.gray,
+    shadowColor: '#000',
   },
-
-  header:{
-    flexDirection:'row',
-    justifyContent:'space-between',
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-
-  userInfo:{
-    flexDirection:'row',
-    alignItems:'center',
-    gap:8,
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
   },
-
-  username:{
-    fontSize:hp(1.7),
-    color:theme.colors.textDark,
-    fontWeight:theme.fonts.bold,
+  username: {
+    fontSize: hp(1.7),
+    color: theme.colors.textDark,
+    fontWeight: theme.fonts.bold,
   },
-
-  postTime:{
-    fontSize:hp(1.4),
-    color:theme.colors.textLight,
-    fontWeight:theme.fonts.medium,
+  postTime: {
+    fontSize: hp(1.4),
+    color: theme.colors.textLight,
+    fontWeight: theme.fonts.medium,
   },
-  content:{
-    gap:10,
+  content: {
+    gap: 10,
   },
-
-  postMedia:{
-    height:hp(40),
-    width:'100%',
-    borderRadius:theme.radius.xl,
-    borderCurve:'continuous',
+  postMedia: {
+    height: hp(40),
+    width: '100%',
+    borderRadius: theme.radius.xl,
+    borderCurve: 'continuous',
   },
-  postBody:{
-    marginLeft:5,
+  postBody: {
+    marginLeft: 5,
   },
-  footer:{
-    flexDirection:'row',
-    alignItems:'center',
-    gap:15,
+  footer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
   },
-
-  footerButton:{
-    marginLeft:5,
-    flexDirection:'row',
-    alignItems:'center',
-    gap:4
+  footerButton: {
+    marginLeft: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
   },
-
-  actions:{
-    flexDirection:'row',
-    alignItems:'center',
-    gap:4
+  count: {
+    color: theme.colors.text,
+    fontSize: hp(1.8),
   },
-  count:{
-    color:theme.colors.text,
-    fontSize:hp(1.8)
-  }
-})
+});
--- services/postServices.js
+++ services/postServices.js
@@ -1,124 +1,90 @@
 import { supabase } from "../lib/supabase";
 import {uploadFile} from "./imageService";
 
-export const createOrUpdatePost = async (post) => {
+export const createorUpdatePost=async(post)=>{
   try {
-    if (post.file && typeof post.file === "object") {
-      let isImage = post?.file?.type === "image";
-      let folderName = isImage ? "postImages" : "postVideos";
-      let fileResult = await uploadFile(folderName, post?.file?.uri, isImage);
-      if (fileResult.success) {
-        post.file = fileResult.data;
-      } else {
+    if(post.file && typeof post.file == 'object'){
+      let isImage=post?.file?.type=='image';
+      let folderName=isImage?'postImages':'postVideos';
+      let fileResult= await uploadFile(folderName,post?.file?.uri,isImage);
+      if(fileResult.success){
+        post.file=fileResult.data;
+      }else{
         return fileResult;
       }
     }
-    const { data, error } = await supabase
-      .from("posts")
-      .upsert(post)
-      .select()
-      .single();
-    if (error) {
+    const{data,error}=await supabase
+    .from('posts')
+    .upsert(post)
+    .select()
+    .single();
+    if(error){
       console.log("Create or update post error", error);
-      return { success: false, msg: "Could not create or update post" };
+    return{success:false,msg:"Could not create or update post"};
     }
-    return { success: true, data };
+    return {success:true,data:data};
   } catch (error) {
     console.log("Create or update post error", error);
-    return { success: false, msg: "Could not create or update post" };
+    return{success:false,msg:"Could not create or update post"};
   }
-};
+}
 
-export const fetchPostDetails = async (postid) => {
+
+export const fetchPost=async(limit=10)=>{
   try {
-    const { data, error } = await supabase
-      .from("posts")
-      .select(
-        `*,
-        user: users(id,name,image),
-        postLikes(*),
-        comments(*, users(id,name,image))`
-      )
-      .eq("id", postid)
-      .order("created_at", { ascending: false, foreignTable: "comments" })
-      .single();
-    if (error) {
-      console.log("fetchPostDetails error", error);
-      return { success: false, msg: "Could not fetch the post" };
+    const {data,error}=await supabase
+    .from('posts')
+    .select(`*
+      ,user: users(id,name,image),
+      postLikes(*)
+      `)
+    .order('created_at', {
+      ascending: false,
+    })
+    .limit(limit);
+    if(error){
+      console.log("fetch post error", error);
+      return{success:false,msg:"Could not fetch the post"};
     }
-    return { success: true, data };
+    return {success:true,data:data};
   } catch (error) {
-    console.log("fetchPostDetails error", error);
-    return { success: false, msg: "Could not fetch the post" };
+    console.log("fetch post error", error);
+    return{success:false,msg:"Could not fetch the post"};
   }
-};
-export const createPostLike = async (postLike)=>{
+}
+export const createPostLike=async(postLike)=>{
   try {
     const {data,error}=await supabase
     .from('postLikes')
     .insert(postLike)
     .select()
     .single();
+
     if(error){
-      console.log("postlike error", error);
-      return{success:false,msg:"Could not like post"};
+      console.log("fetch post error", error);
+      return{success:false,msg:"Could not like the post"};
     }
     return {success:true,data:data};
   } catch (error) {
-    console.log('postlike error', error);
-    return{success:false,msg:"Could not like post"};
+    console.log("fetch post error", error);
+    return{success:false,msg:"Could not like the post"};
   }
 }
-export const removePostLike = async (postLike)=>{
+export const removePostLike=async(postId,userId)=>{
   try {
     const {error}=await supabase
     .from('postLikes')
     .delete()
-    .eq('userId',userId)
     .eq('postId',postId)
+    .eq('userId',userId)
+
     if(error){
-      console.log("postlike error", error);
-      return{success:false,msg:"Could not remove the like"};
+      console.log("postLike error", error);
+      return{success:false,msg:"Could not remove the post"};
     }
     return {success:true};
   } catch (error) {
-    console.log('postlike error', error);
-    return{success:false,msg:"Could not remove the like"};
-  }
-}
-
-export const createComment = async (comment)=>{
-  try {
-    const {data,error}=await supabase
-    .from('comments')
-    .insert(comment)
-    .select()
-    .single();
-    if(error){
-      console.log("comment error", error);
-      return{success:false,msg:"Could not create comment"};
-    }
-    return {success:true,data:data};
-  } catch (error) {
-    console.log('comment error', error);
-    return{success:false,msg:"Could not create comment"};
-  }
-}
-
-export const removeComment = async (commentId)=>{
-  try {
-    const {error}=await supabase
-    .from('comments ')
-    .delete()
-    .eq('id',commentId)
-
-    if(error){
-      console.log("removeComment error", error);
-      return{success:false,msg:"Could not remove the comment"};
-    }
-    return {success:true,data:{commentId}};
-  } catch (error) {
-    console.log('removeComment error', error);
-    return{success:false,msg:"Could not remove the comment"};
+    console.log("postLike error", error);
+    return{success:false,msg:"Could not remove the post"};
   }
 }
